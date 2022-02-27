d27c65fe8313d32681b53d2bfa17f70e38d9cd48
Miguel Boubeta <boubeta.miguel@gmail.com>
2022-02-27 12:00:51 +0100

    feat: integrate the ebp of v and p into a single function

--- R/apoiss-predictors.R
+++ R/apoiss-predictors.R
@@ -10,47 +10,12 @@
 
 log_pd = function(X, d, beta, phi, vd) {
   
-  log_pd = X[d, ] %*% beta + phi*vd
-  return(log_pd)
+  logpd = c(X[d, ] %*% beta) + phi*vd
+  return(logpd)
 }
 
 
-#' Calculate the EBP of p_d.
-#'
-#' @param y: Response variable.
-#' @param X: Design matrix.
-#' @param beta: Beta parameter.
-#' @param phi: Variance component.
-#' @param nu: Offset parameter.
-#' @param L: length of the simulated random effects.
-#' @return The EBP of p_d.
-#' @export
-#' @examples
-#' EBPp(y, X, beta, phi, nu, L)
-
-EBPp = function(y, X, beta, phi, nu, L) {
-  
-  D = dim(X)[1]
-  p_ebp = numeric(D)
-  
-  for (d in 1:D) {
-    
-    # generate random effects
-    vd = rnorm(L, mean=0, sd=1)
-    vd = c(vd,-vd)
-    
-    # calculate the components of the EBP pf pd (Nd and Dd) 
-    logpd = log_pd(X, d, beta, phi, vd)
-    Nd = sum(exp((y[d] + 1) * logpd - nu[d] * exp(logpd))) / (2*L)
-    Dd = sum(exp(y[d] * logpd - nu[d] * exp(logpd))) / (2*L)
-    p_ebp[d] = Nd / Dd
-  }
-  
-  return(p_ebp)
-}	
-
-
-#' Calculate the EBP of v_d.
+#' Calculate the EBP of p_d or v_d.
 #'
 #' @param d: Index of the domain.
 #' @param y: Response variable.
@@ -59,23 +24,35 @@ EBPp = function(y, X, beta, phi, nu, L) {
 #' @param phi: Variance component.
 #' @param nu: Offset parameter.
 #' @param L: length of the simulated random effects.
-#' @return The EBP of v_d.
+#' @param parameter: 'target' for p_d and 'random_effects' for v_d.
+#' @return The EBP of p_d or v_d.
 #' @export
 #' @examples
-#' EBPvd(d, y, X, beta, phi, nu, L)
+#' ebpd(d, y, X, beta, phi, nu, L, parameter)
 
-EBPvd = function(d, y, X, beta, phi, nu, L) {
+ebpd = function(d, y, X, beta, phi, nu, L, parameter) {
   
-  vl = rnorm(L, mean=0, sd=1)
-  vl = c(vl, -vl)
-  N = sum(vl * exp(y[d] * (X[d, ] %*% beta + phi * vl) - nu[d] * exp(X[d, ] %*% beta + phi * vl))) / (2*L)
-  D = sum(exp(y[d] * (X[d, ] %*% beta + phi * vl) - nu[d] * exp(X[d, ] %*% beta + phi * vl))) / (2*L)
-  vd_ebp = N / D
-  return(vd_ebp)
+  vd = rnorm(L, mean=0, sd=1)
+  vd = c(vd, -vd)
+  
+  # calculate the components of the EBP pf pd (Nd and Dd) 
+  logpd = log_pd(X, d, beta, phi, vd)
+  
+  if (parameter == 'target') {
+    Nd = sum(exp((y[d] + 1) * logpd - nu[d] * exp(logpd))) / (2*L)
+  } else if (parameter == 'random_effects') {
+    Nd = sum(vd * exp(y[d] * logpd - nu[d] * exp(logpd))) / (2*L)
+  } else {
+    stop("parameter not valid!")
+  }
+  
+  Dd = sum(exp(y[d] * logpd - nu[d] * exp(logpd))) / (2*L)
+  ebpd = Nd / Dd
+  return(ebpd)
 }
 
 
-#' Calculate the EBP of v.
+#' Calculate the EBP of p or v.
 #'
 #' @param y: Response variable.
 #' @param X: Design matrix.
@@ -83,17 +60,19 @@ EBPvd = function(d, y, X, beta, phi, nu, L) {
 #' @param phi: Variance component.
 #' @param nu: Offset parameter.
 #' @param L: length of the simulated random effects.
-#' @return The EBP of v.
+#' @param parameter: 'target' for p and 'random_effects' for v.
+#' @return The EBP of p or v.
 #' @export
 #' @examples
-#' EBPv(y, X, beta, phi, nu, L)
+#' ebp(y, X, beta, phi, nu, L, parameter)
 
-EBPv = function(y, X, beta, phi, nu, L) {
+ebp = function(y, X, beta, phi, nu, L, parameter) {
   
   D = dim(X)[1]
   d = 1:D
-  v_ebp = sapply(d, EBPvd, y, X, beta, phi, nu, L)
-  return(v_ebp)
+  
+  ebp = sapply(d, ebpd, y, X, beta, phi, nu, L, parameter)
+  return(ebp)
 }
   
   
@@ -106,12 +85,12 @@ EBPv = function(y, X, beta, phi, nu, L) {
 #' @return The plug-in estimator of p.
 #' @export
 #' @examples
-#' PLUGINp(X, beta, phi, v)
+#' plugin(X, beta, phi, v)
 
-PLUGINp = function(X, beta, phi, v) {
+plugin = function(X, beta, phi, v) {
   
-  p_plugin = exp(X %*% beta + phi * v)
-  return(p_plugin)
+  plug = exp(X %*% beta + phi * v)
+  return(plug)
 }
 
 
